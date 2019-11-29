7e856f66f06fc9b66c57f14c5b2285f1c35b9481
Gabarros <gabarros17@gmail.com>
2019-11-29 17:41:57 -0300

    Crate Hotel Controller

--- controllers/HotelController.js
+++ controllers/HotelController.js
@@ -2,8 +2,18 @@ const Hotel = require('../models/Hotel');
 
 class HotelController{
 
+    constructor(){
+        let hotelsList = [];
+    }
+
     createNewHotel(name, classification, workweekDaysTax, weekendDays){
 
-        new Hotel(name, classification, workweekDaysTax, weekendDays);
+        let hotel = new Hotel(name, classification, workweekDaysTax, weekendDays);
+        let name = hotel.name;
+
+
+        this.hotelsList.push({name: hotel});
     }
+
+
 }
